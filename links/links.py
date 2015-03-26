749811bb8912e4108e636197d2f10b6ffd631652
Your Name“git config user.name xristalla“ <your github registered e-mailgit config user.email xristalla_92@hotmail.com>
2015-03-26 22:14:50 +0200

    allages

--- links/links.py
+++ links/links.py
@@ -3,7 +3,7 @@
 import pkg_resources
 
 from xblock.core import XBlock
-from xblock.fields import Scope, Integer
+from xblock.fields import Scope, String
 from xblock.fragment import Fragment
 
 
@@ -16,16 +16,15 @@ class LinksXBlock(XBlock):
     # self.<fieldname>.
 
     # TO-DO: delete count, and define your own fields.
-    count = Integer(
-        default=0, scope=Scope.user_state,
-        help="A simple counter, to show something happening",
-    )
+    href1 = String(help="First URL", default=None, scope=Scope.content)
+    href2 = String(help="Second URL", default=None, scope=Scope.content)
+    href3 = String(help="Third URL", default=None, scope=Scope.content)
 
     def resource_string(self, path):
         """Handy helper for getting resources from our kit."""
         data = pkg_resources.resource_string(__name__, path)
         return data.decode("utf8")
-
+		
     # TO-DO: change this view to display your data your own way.
     def student_view(self, context=None):
         """
@@ -41,28 +40,30 @@ class LinksXBlock(XBlock):
 
     # TO-DO: change this handler to perform your own actions.  You may need more
     # than one handler, or you may not need any handlers at all.
-    @XBlock.json_handler
-    def increment_count(self, data, suffix=''):
-        """
-        An example handler, which increments the data.
-        """
-        # Just to show data coming in...
-        assert data['hello'] == 'world'
-
-        self.count += 1
-        return {"count": self.count}
-
-    # TO-DO: change this to create the scenarios you'd like to see in the
-    # workbench while developing your XBlock.
     @staticmethod
     def workbench_scenarios():
         """A canned scenario for display in the workbench."""
         return [
-            ("LinksXBlock",
-             """<vertical_demo>
-                <links/>
-                <links/>
-                <links/>
-                </vertical_demo>
-             """),
-        ]
+           ("LinksXBlock",
+		   """<links/>"""),
+			]
+
+    def studio_view(self, context=None):
+        html = self.resource_string("static/html/links_edit.html")
+        href1 = self.href1 or ''
+        href2 = self.href2 or ''
+        href3 = self.href3 or ''
+        frag = Fragment(html.format(href1=href1,href2=href2,href3=href3,self=self))
+        frag.add_javascript(self.resource_string("static/js/src/links_edit.js"))
+        frag.initialize_js('LinksEditXBlock')
+        return frag
+
+    @XBlock.json_handler
+    def studio_submit(self, data, suffix=''):
+        self.href1 = data['href1']
+        self.href2 = data['href2']
+        self.href3 = data['href3']
+
+        return {
+            'result': 'success',
+        }
